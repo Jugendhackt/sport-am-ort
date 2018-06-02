58a199425c1ccc292c1f77d2324df8d9b514aac1
ibrahim <ibr.12399@gmail.com>
2018-06-02 15:09:39 +0200

    einloggen funktioniert mit sport / ort

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function einloggen(){
-	alert('es funktioniert');
+var username = document.getElementById("username").value;
+var passwort = document.getElementById("passwort").value;
+var params = {
+		
+		url:'http://localHost:8080/function/einloggen?username='+ username + '&passwort='+ passwort,
+		sucsess:function(result){alert("JAAA")}
+		
+	}
+jQuery.ajax(params);
 	
 	
 }
